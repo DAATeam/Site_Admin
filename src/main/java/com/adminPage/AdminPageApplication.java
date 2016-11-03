e6110652884b4e1db4b17ee09d361e865778f069
conandk <ddangkhoa94@gmail.com>
2016-11-03 17:37:11 +0700

    init project SiteAdmin with SpringBoot

---
+++ src/main/java/com/adminPage/AdminPageApplication.java
@@ -0,0 +1,12 @@
+package com.adminPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPageApplication.class, args);
+	}
+}
